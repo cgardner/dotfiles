0291d86d3d70f13d136510d83ff4b77d09a380b0
Craig Gardner <craig.gardner@cdk.com>
2019-04-30 23:27:53 -0700

    fixed zsh prompt

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -15,7 +15,9 @@ compinit
 
 ZSH_ROOT="$HOME/src/dotfiles/zsh"
 ZSH_PLUGIN_DIR="$ZSH_ROOT/plugins"
+source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/git.zsh
 source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 source $ZSH_ROOT/aliases.sh
+source $ZSH_ROOT/prompt.sh
