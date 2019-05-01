647813df23ea399ad65f455186c1b242b001b698
Craig Gardner <craig.gardner@cdk.com>
2019-05-01 07:08:13 -0700

    better oh-my-zsh libraries

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -11,11 +11,22 @@ for plugin ($plugins); do
     fpath=(~/dotfiles/zsh/plugins/oh-my-zsh/plugins/$plugin $fpath)
 done
 
+zsh_lib=(
+  git
+  history
+  completion
+  key-bindings
+)
+
 compinit
 
 ZSH_ROOT="$HOME/src/dotfiles/zsh"
 ZSH_PLUGIN_DIR="$ZSH_ROOT/plugins"
-source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/git.zsh
+
+for lib ($zsh_lib); do
+    source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/$lib.zsh
+done
+
 source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH_PLUGIN_DIR/zsh-touchbar/zsh-iterm-touchbar.plugin.zsh
