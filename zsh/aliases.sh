4c9d72d705f11647077a92c572de56107653843d
Craig Gardner <craig.gardner@cdk.com>
2019-05-01 12:15:12 -0700

    added some more helpful aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,5 +1,4 @@
 alias g="git"
-alias mux="tmuxinator"
 alias zref="source $HOME/.zshrc"
 
 ## NODE
@@ -8,3 +7,9 @@ alias ni="npm i"
 
 ## UPGRAYEDD
 alias upgrayedd='HOMEBREW_INSTALL_CLEANUP=true brew upgrade && brew cask upgrade && brew cleanup && mas upgrade'
+
+## Tmuxinator
+alias mux="tmuxinator"
+alias p="tmuxinator start project "
+
+alias pl="ls $HOME/src"
