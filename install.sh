c13ae6667f06739dd2f7ee6c98bdb20597a750d1
Craig Gardner <craig.gardner@cdk.com>
2019-05-01 11:37:56 -0700

    using variables instead of tilde

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@
 # Install Dein for vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 # For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein && rm ./installer.sh
+sh ./installer.sh $HOME/.cache/dein && rm ./installer.sh
 
-printf "so $HOME/src/dotfiles/vim/vimrc.vim" > ~/.vimrc
+printf "so $HOME/src/dotfiles/vim/vimrc.vim" > $HOME/.vimrc
 
 ### END VIM SETUP
 
 ### TMUX SETUP
-
-printf "source-file $HOME/src/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+printf "source-file $HOME/src/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
 ### END TMUX SETUP
+
+### ZSH SETUP
+printf "source $HOME/src/dotfiles/zsh/zshrc-manager.sh" > $HOME/.zshrc
+### END ZSH SETUP
