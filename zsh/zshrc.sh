b2c754938fd66273f7b8181eb77f3943aad97e9c
Craig Gardner <craig.gardner@cdk.com>
2019-04-30 23:43:41 -0700

    autojump integration

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -18,6 +18,7 @@ ZSH_PLUGIN_DIR="$ZSH_ROOT/plugins"
 source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/git.zsh
 source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
 source $ZSH_ROOT/aliases.sh
 source $ZSH_ROOT/prompt.sh
