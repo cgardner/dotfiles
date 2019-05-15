3942ac9e94d9b0c27fd081ea3da9ea39f86f278b
Craig Gardner <craig.gardner@cdk.com>
2019-05-15 07:03:55 -0700

    zsh updates and performance optimizations

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -25,15 +25,34 @@ PATH="/usr/local/bin:$PATH"
 ZSH_ROOT="$HOME/src/dotfiles/zsh"
 ZSH_PLUGIN_DIR="$ZSH_ROOT/plugins"
 
+function load_source() {
+  DEBUG=$2
+  #DEBUG=true
+
+  if [ -z $DEBUG ]; then
+    source $1
+    return
+  fi
+
+  local sourceFile=$1
+  timer=$(($(gdate +%s%N)/1000000)) 
+
+  source $sourceFile
+
+  now=$(($(gdate +%s%N)/1000000))
+  elapsed=$(($now-$timer))
+  echo $elapsed":" $sourceFile
+}
+
 for lib ($zsh_lib); do
-    source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/$lib.zsh
+  load_source $ZSH_PLUGIN_DIR/oh-my-zsh/lib/$lib.zsh
 done
 
-source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+load_source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+load_source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH_PLUGIN_DIR/zsh-touchbar/zsh-iterm-touchbar.plugin.zsh
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
-source $ZSH_ROOT/aliases.sh
-source $ZSH_ROOT/prompt.sh
-source $ZSH_ROOT/keybindings.sh
+load_source $ZSH_ROOT/aliases.sh
+load_source $ZSH_ROOT/prompt.sh
+load_source $ZSH_ROOT/keybindings.sh
