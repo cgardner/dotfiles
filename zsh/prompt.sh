e1a70b0ffd6c6fa9f82bd15bf1eaea44d13f5c0f
Craig Gardner <craig.gardner@cdk.com>
2019-07-08 08:17:32 -0700

    sped up my terminal by using powerlevel10k

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,6 +1,9 @@
 POWERLEVEL9K_MODE='nerdfont-complete'
 
-load_source $HOME/src/dotfiles/zsh/plugins/powerlevel9k/powerlevel9k.zsh-theme
-
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
 POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_VCS_BACKENDS=(git)
+POWERLEVEL9K_VCS_MAX_SYNC_LATENCY_SECONDS=.05
+
+load_source $ZSH_PLUGIN_DIR/powerlevel10k/powerlevel10k.zsh-theme
+load_source $ZSH_PLUGIN_DIR/zsh-prompt-benchmark/zsh-prompt-benchmark.plugin.zsh
