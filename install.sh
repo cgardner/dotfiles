2a7c1a54a188c426eb13056a2cf4d5277dfa39b6
Craig Gardner <craig.s.gardner@gmail.com>
2019-12-02 11:27:24 -0800

    feat: installing homebrew first

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew install rust
 cargo install --git https://github.com/ubnt-intrepid/dot.git
 dot init cgardner/dotfiles
