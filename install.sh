f099b68a77da42b48bd63626679f3bcbdae77d81
Craig Gardner <craig.s.gardner@gmail.com>
2019-12-03 22:11:13 -0800

    fix: fixing the path to dot since the updated path isnt set yet

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 brew install rust
 cargo install --git https://github.com/ubnt-intrepid/dot.git
-dot init cgardner/dotfiles
+$HOME/.cargo/bin/dot init cgardner/dotfiles
 
 xargs brew install < $HOME/.dotfiles/brew
 xargs brew cask install < $HOME/.dotfiles/brew-cask
