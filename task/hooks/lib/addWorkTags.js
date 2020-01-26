f1ed526aa79201f0df8cb6f23df418570e665884
Craig Gardner <craig.s.gardner@gmail.com>
2020-01-26 14:58:56 +0530

    feat(taskwarrior): added hooks for adding defaults

---
+++ task/hooks/lib/addWorkTags.js
@@ -0,0 +1,14 @@
+const addWorkTags = task => {
+  if (task.tags.includes("@email") && task.tags.includes("work")) {
+    const tags = [...task.tags, "@vpn", "@computer"].reduce((acc, cur) => {
+      if (!acc.includes(cur)) {
+        acc.push(cur);
+      }
+      return acc;
+    }, []);
+    task.tags = tags;
+  }
+
+  return task;
+};
+module.exports = addWorkTags;
