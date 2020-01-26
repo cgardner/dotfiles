1e90170aaa531b9a7c8986b9af632698fd6d801d
Craig Gardner <craig.s.gardner@gmail.com>
2020-01-26 15:30:05 +0530

    fix(taskwarrior): added a check for tags before changing them

--- task/hooks/lib/addWorkTags.js
+++ task/hooks/lib/addWorkTags.js
@@ -1,4 +1,7 @@
 const addWorkTags = task => {
+  if (!task.hasOwnProperty("tags")) {
+    return task;
+  }
   if (task.tags.includes("@email") && task.tags.includes("work")) {
     const tags = [...task.tags, "@vpn", "@computer"].reduce((acc, cur) => {
       if (!acc.includes(cur)) {
