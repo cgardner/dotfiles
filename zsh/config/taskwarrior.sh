20a3c08cbdb0d3bd8e683a6a66b52ec1241afec6
Craig Gardner <craig.s.gardner@gmail.com>
2020-01-26 15:19:27 +0530

    fix(zsh): cleaned up taskwarrior aliases

--- zsh/config/taskwarrior.sh
+++ zsh/config/taskwarrior.sh
@@ -1,19 +1,5 @@
-tickle () {
-    deadline=$1
-    shift
-    in +tickle wait:$deadline $@
-}
-
-alias t=task
-alias tick=tickle
-alias think="tickle +1d"
-
+alias t="task"
 alias th="taskhelper"
-alias in="taskhelper in"
-alias greg="taskhelper greg"
-alias northon="taskhelper northon"
-alias amplitude="taskhelper amplitude"
-alias rethink="taskhelper rethink"
-alias upcoming="t due.before:\"$(date -v "+7d" +"%Y-%m-%d")\""
 
 alias tw="timew"
+
